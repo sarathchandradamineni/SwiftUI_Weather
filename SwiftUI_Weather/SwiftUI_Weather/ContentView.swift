3767cd368e066ed6c08d6c21e3ed1b2340206697
sarathchandradamineni <daminenisarathchandra@gmail.com>
2021-08-24 21:06:02 +0200

    My first commit in SwiftUI Weather application

---
+++ SwiftUI_Weather/SwiftUI_Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Weather
+//
+//  Created by Sarath Chandra Damineni on 2021-08-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
